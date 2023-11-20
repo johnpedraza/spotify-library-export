d83e9001489e2cd28a47b16845b659787854ce3d
JohnPedraza <jpedraza1123@gmail.com>
2023-11-20 18:21:56 -0500

    add JSON download functionality

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -39,6 +39,32 @@ const HomePage = () => {
     }
   };
 
+  const handleExportToJson = () => {
+    try {
+      // Convert libraryData to a JSON string
+      const jsonString = JSON.stringify(libraryData, null, 2);
+
+      // Create a Blob containing the JSON string
+      const blob = new Blob([jsonString], { type: 'application/json' });
+
+      // Create a download link
+      const link = document.createElement('a');
+      link.href = URL.createObjectURL(blob);
+
+      // Set the filename for the download
+      link.download = 'libraryData.json';
+
+      // Append the link to the document and trigger the click event
+      document.body.appendChild(link);
+      link.click();
+
+      // Remove the link from the document
+      document.body.removeChild(link);
+    } catch (error) {
+      console.error('Error exporting to JSON:', error);
+    }
+  };
+
   return (
     <div>
       <h1>Spotify Exporter</h1>
@@ -50,6 +76,8 @@ const HomePage = () => {
           </button>
 
           {libraryData.length > 0 && (
+            <>
+            <button onClick={handleExportToJson}>Export to JSON</button>
             <div>
               <h2>Library Data:</h2>
               <ul>
@@ -60,6 +88,7 @@ const HomePage = () => {
                 ))}
               </ul>
             </div>
+            </>
           )}
 
         <button onClick={handleLogout}>Logout</button>

7b4813e55255098d4cf03aa59a02a748cfd2e29f
JohnPedraza <jpedraza1123@gmail.com>
2023-11-20 09:36:44 -0500

    add basic Spotify auth

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import { loginUrl, setAccessToken } from '../services/SpotifyAuth';
 
 const HomePage = () => {
+  const handleLogin = () => {
+    window.location.href = loginUrl;
+  };
+
+  const handleLogout = () => {
+    window.location.href = 'www.google.com'; // placeholder
+  };
+
+  // Check if the token is present in the URL and set it
+  const isTokenSet = setAccessToken();
+
   return (
     <div>
       <h1>Spotify Exporter</h1>
+      {isTokenSet ? (
+        <button onClick={handleLogout}>Logout</button>
+      ) : (
+        <button onClick={handleLogin}>Login with Spotify</button>
+      )}
     </div>
   );
 };
---
+++ src/services/SpotifyAuth.js
@@ -0,0 +1,36 @@
+import SpotifyWebApi from 'spotify-web-api-js';
+
+const spotifyApi = new SpotifyWebApi();
+const redirectUri = process.env.REACT_APP_REDIRECT_URI
+
+export const authEndpoint = 'https://accounts.spotify.com/authorize';
+const clientId = process.env.REACT_APP_SPOTIFY_CLIENT_ID
+
+// Redirect the user to the Spotify authorization page
+export const loginUrl = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&response_type=token&show_dialog=true`;
+
+// Parse the token from the URL after successful authorization
+export const getTokenFromUrl = () => {
+  return window.location.hash
+    .substring(1)
+    .split('&')
+    .reduce((initial, item) => {
+      let parts = item.split('=');
+      initial[parts[0]] = decodeURIComponent(parts[1]);
+
+      return initial;
+    }, {});
+};
+
+// Set the token to the Spotify API instance
+export const setAccessToken = () => {
+  const token = getTokenFromUrl();
+  console.log(token)
+  if (token.access_token) {
+    spotifyApi.setAccessToken(token.access_token);
+    return true;
+  }
+  return false;
+};
+
+export default spotifyApi;
